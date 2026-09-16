083f8315aa9d9b41e505cc3eb5792d9eb6bb5a8e
Virgo Jahtra Sinaga <virgosinaga05@gmail.com>
2026-09-16 09:08:49 +0700

    Initial commit: EO event report & analytics app

--- js/auth-config.js
+++ js/auth-config.js
@@ -8,7 +8,7 @@ const AUTH_CONFIG = {
   // Tempel URL /exec dari deployment Login.gs (Web App terpisah) di sini,
   // contoh: "https://script.google.com/macros/s/AKfycb.../exec".
   // Kosongkan untuk mode demo lokal (lihat catatan di login.js).
-  LOGIN_API_URL: "https://script.google.com/macros/s/AKfycbzt6wlvafab9mtbUcnFsEvWm3uHXCXXlmZbQC3LbPsqqXoiAPunfYYUBVtkUR3ixvaX/exec",
+  LOGIN_API_URL: "https://script.google.com/macros/s/AKfycbxbkxKB0m9DlyE1xnxx_ncmAlS5rqKfcoMx3wtBrDANcl_FoP7IEyurgCXpPfoNeG81/exec",
 
   // Key sessionStorage yang menandai user sudah login di tab browser ini.
   SESSION_KEY: "maximum_auth_session",
